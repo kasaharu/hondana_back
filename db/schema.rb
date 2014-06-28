108092de77f0b1867f90fc7c1852bb3c457e25d4
Wataru.Kasahara <kasahara1019@gmail.com>
2014-06-28 22:42:58 +0900

    Implement basic user function.

--- db/schema.rb
+++ db/schema.rb
@@ -43,8 +43,14 @@ ActiveRecord::Schema.define(version: 20140624143129) do
   add_index "books", ["name"], name: "index_books_on_name", unique: true, using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",              default: "", null: false
-    t.string   "encrypted_password", default: "", null: false
+    t.string   "email",               default: "", null: false
+    t.string   "encrypted_password",  default: "", null: false
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",       default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "bookrack_id"
