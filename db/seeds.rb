98c74510e7209d52eb71ca7fc837cbf9a7371ff7
Wataru.Kasahara <kasahara1019@gmail.com>
2014-12-08 23:55:48 +0900

    Add temporary data of Bookrack and Book.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,33 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create(
-  :email    => 'hoge@test.com',
-  :password => 'hogehoge'
+  :email    => "hoge@test.com",
+  :password => "hogehoge"
 )
 
+3.times do |i|
+  Bookrack.create(
+    :name    => "bookrack_#{i}",
+    :user_id => "1"
+  )
+end
+
+5.times do |i|
+  Book.create(
+    :name        => "book_#{i}",
+    :author      => "author_#{i}",
+    :category    => "category_#{i}",
+    :memo        => "hogehogehoge_#{i}",
+    :bookrack_id => "1"
+  )
+end
+
+15.times do |i|
+  Book.create(
+    :name        => "book_2_#{i}",
+    :author      => "author_#{i}",
+    :category    => "category_#{i}",
+    :memo        => "hogehogehoge_#{i}",
+    :bookrack_id => "2"
+  )
+end
